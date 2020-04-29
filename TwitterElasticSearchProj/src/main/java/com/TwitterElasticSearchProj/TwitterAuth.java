d8fcb666ab4477d771081f72b79e661e452ca6c9
Gregory Capra <gregoryrcapra@gmail.com>
2020-04-29 11:39:56 -0700

    removing secret auth from file

--- TwitterElasticSearchProj/src/main/java/com/TwitterElasticSearchProj/TwitterAuth.java
+++ TwitterElasticSearchProj/src/main/java/com/TwitterElasticSearchProj/TwitterAuth.java
@@ -6,8 +6,8 @@ public final class TwitterAuth {
         // restrict instantiation
     }
 
-    public static final String CONSUMER_KEY = "vLcvlLMZujpE2Iix9FuB0ZGoD";
-    public static final String CONSUMER_SECRET = "f8e9WWvYpv8Mo4i46iwZTPUgCLA4WBjfP6Z8qV3xrTKnFoU8ZX";
-    public static final String TOKEN = "3905096810-qXeu860dgF0ASvzUVOiiyl5RGjlBaYXLV4melwz";
-    public static final String SECRET = "BNQ3Chav4qgNryTR0cnc4bYbK73aMNpgkXBP5Ajd8b3Pv";
+    public static final String CONSUMER_KEY = "";
+    public static final String CONSUMER_SECRET = "";
+    public static final String TOKEN = "";
+    public static final String SECRET = "";
 }
